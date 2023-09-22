72957d819e3a4156282fd95281a390ae56a4b201
Yutassk <sasaki.yt01@gmail.com>
2023-09-22 10:36:15 +0900

    🎨 Resultページの "QuizProvider" 外した

--- src/app/Result/page.tsx
+++ src/app/Result/page.tsx
@@ -2,18 +2,16 @@
 import React, { useContext } from "react";
 import Footer from "../layout/Footer";
 import Header from "../layout/Header";
-import { QuizContext, QuizProvider } from "../component/QuizProvider";
+import { QuizContext } from "../component/QuizProvider";
 
 const Result = () => {
   const { score, exam } = useContext(QuizContext);
 
   return (
     <div>
-      <QuizProvider>
-        <Header />
-        <h2 className="text-3xl items-center text-center my-6">{`${exam}問中 : ${score}問正解！`} </h2>
-        <Footer />
-      </QuizProvider>
+      <Header />
+      <h2 className="text-3xl items-center text-center my-6">{`${exam}問中 : ${score}問正解！`} </h2>
+      <Footer />
     </div>
   );
 };
