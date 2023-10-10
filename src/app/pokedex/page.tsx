eec6b512af56772625aaf5f472be350b8f20d968
Yutassk <sasaki.yt01@gmail.com>
2023-10-10 18:34:11 +0900

    🎨  モーダルにタイプも表示されるように

--- src/app/pokedex/page.tsx
+++ src/app/pokedex/page.tsx
@@ -8,6 +8,9 @@ import InfiniteScroll from "react-infinite-scroller";
 
 export default function Home() {
   const [pokeIndex, setPokeIndex] = useState<number[]>([1, 2, 3, 4]);
+  const [modal, setModal] = useState<number>(0);
+  const [pokeName, setPokeName] = useState<string>("");
+  const [pokeType, setPokeType] = useState<string[]>([]);
 
   const loadMore = () => {
     const nextIndex = [];
@@ -18,8 +21,40 @@ export default function Home() {
     setPokeIndex([...pokeIndex, ...nextIndex]);
   };
 
-  const handlePokeDetail = (pokeIndex: number) => {
-    console.log(pokeIndex);
+  const fetchPokemonData = async (props: number | string) => {
+    try {
+      // ポケモンの全データ
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${props}/`);
+      const data = await response.json();
+
+      // ポケモンの名前取得
+      const fetchPokemonName = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${data.species.name}/`);
+      const fetchPokemonNameData = await fetchPokemonName.json();
+      const japaneseName = fetchPokemonNameData.names.find((name: { language: { name: string } }) => name.language.name === "ja").name;
+      setPokeName(japaneseName);
+
+      // ポケモンのタイプ取得
+      data.types.map(async (slot: { type: { name: string } }) => {
+        const fetchPokemonType = await fetch(`https://pokeapi.co/api/v2/type/${slot.type.name}/`);
+        const fetchPokemonTypeData = await fetchPokemonType.json();
+        const jaType = fetchPokemonTypeData.names.find((name: { language: { name: string } }) => name.language.name === "ja").name;
+        setPokeType((prevPokeType) => [...prevPokeType, jaType]);
+      });
+    } catch (error) {
+      console.error("Error fetching Pokemon name:", error);
+      return "";
+    }
+  };
+
+  const handlePokeDetail = (index: number) => {
+    setModal(index);
+    fetchPokemonData(index);
+  };
+
+  const closedModal = () => {
+    setModal(0);
+    setPokeName("");
+    setPokeType([]);
   };
 
   const items = (
@@ -55,9 +90,25 @@ export default function Home() {
         <p>好きなポケモンを登録しよう</p>
       </div>
 
+      {modal > 0 && (
+        <div className="group" onClick={closedModal}>
+          <div className="block w-full h-full bg-black/70 fixed top-0 left-0">
+            <div className="block w-3/4 mx-auto mt-20 bg-white">
+              <Image width={200} height={200} src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${modal}.png`} alt="" />
+              <p>{pokeName}</p>
+              {pokeType.map((type) => (
+                <p key={type}>{type}</p>
+              ))}
+              <button onClick={() => console.log(pokeType)}>koko</button>
+            </div>
+          </div>
+        </div>
+      )}
+
       <InfiniteScroll loadMore={loadMore} hasMore={true} loader={loader}>
         {items}
       </InfiniteScroll>
+
       <Footer />
     </div>
   );
