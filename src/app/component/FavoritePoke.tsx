d8877263eead1bfc62e808a54407221641cbf3a2
Yutassk <sasaki.yt01@gmail.com>
2023-10-13 16:07:36 +0900

    ✨ お気に入り登録したポケモンをマイページで表示

--- src/app/component/FavoritePoke.tsx
+++ src/app/component/FavoritePoke.tsx
@@ -1,27 +1,33 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "./AuthProvider";
 import { db } from "./Firebase";
-import { collection, doc, getDocs, query } from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
+import Image from "next/image";
 
 export const FavoritePoke = () => {
   const { uid } = useContext(AuthContext);
+  const [favoritePoke, setFavoritePoke] = useState({
+    num: 0,
+    name: "",
+  });
 
   useEffect(() => {
     if (uid) {
       const fetchFavorite = async () => {
-        const usersRef = collection(db, "users");
-        const userDocRef = doc(usersRef, uid);
-        const favoriteRef = collection(userDocRef, "favorite");
-
         try {
-          const q = query(favoriteRef);
-          const querySnapshot = await getDocs(q);
-
-          querySnapshot.forEach((doc) => {
-            const data = doc.data();
-            console.log(data.favorite);
+          const usersRef = doc(db, "users", uid);
+          const docSnap = (await getDoc(usersRef)).data()!;
+          setFavoritePoke((prevData) => {
+            return { ...prevData, num: docSnap.favorite };
           });
-          //   setRecords(updatedRecord);
+
+          // ポケモンの名前取得
+          const fetchPokemonName = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${docSnap.favorite}/`);
+          const fetchPokemonNameData = await fetchPokemonName.json();
+          const japaneseName = fetchPokemonNameData.names.find((name: { language: { name: string } }) => name.language.name === "ja").name;
+          setFavoritePoke((prevData) => {
+            return { ...prevData, name: japaneseName };
+          });
         } catch (e) {
           console.error("Error fetching results:", e);
           return [];
@@ -30,5 +36,16 @@ export const FavoritePoke = () => {
       fetchFavorite();
     }
   }, [uid]);
-  return <div>FavoritePoke</div>;
+  return (
+    <div>
+      <div>
+        {favoritePoke.num && (
+          <div>
+            <Image width={200} height={200} src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${favoritePoke.num}.png`} alt="" />
+            <p>{favoritePoke.name}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
